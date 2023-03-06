951488db5a478debec98cd653fac0d76ca2afe7e
Kyle Wehrung <kyle.wehrung@gmail.com>
2023-03-06 04:38:10 -0700

    some cli changes, messy

--- lib/cli.py
+++ lib/cli.py
@@ -1,12 +1,12 @@
 from db.models import Base, KyleItem, KyleLog
 import random
-from helpers import create_table
+from helpers import create_table, fill_kyle_cart
 
 from faker import Faker
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 
-engine = create_engine('sqlite:///kyle.db')
+engine = create_engine('sqlite:///db/kyle.db')
 Base.metadata.create_all(engine)
 
 Session = sessionmaker(bind=engine)
@@ -26,17 +26,32 @@ if __name__ == '__main__':
     =============================================================================
     ''')
     print('Hello! Welcome to Only Kyles, the Kyle orphanage by Kyles that finds Kyles for Kyles.')
+
+    #check if name is kyle, if not raise exception
     name = input('Please enter your first and last name: ')
     [first_name, last_name] = name.split(' ')
     if not (first_name.lower() == 'kyle'):
         raise Exception('We cannot permit the sale of Kyle orphans to individuals not named Kyle.')
     
+    #display list of KyleItems
     print(f"Welcome {name} take a look at our merchandise")
     kyles = session.query(KyleItem)
-    print(kyles.first())
+   
     create_table(kyles)
+    
+    fill_kyle_cart(session, "kyle")
 
-    kyle = None
-    while not kyle:
-        kyle_id = input('Please enter the ID of the Kyle you wish to purchase: ')
-        kyle = session.query(Store).filter(Store.id == store_id).one_or_none()
+        
+    #display information for the kyle selected
+    # print('Here is some more information on the Kyle you selected:')
+    # print(kyle)    
+    # print("Would you like to continue shopping for Kyles?")
+     
+
+    
+
+
+
+
+
+
--- lib/helpers.py
+++ lib/helpers.py
@@ -1,4 +1,5 @@
 import pandas as pd
+from db.models import Base, KyleItem, KyleLog, KyleCart
 
 def create_table(kyles):
     dtf = pd.DataFrame(columns=['id', 'name', 'superpower'])
@@ -9,5 +10,41 @@ def create_table(kyles):
     print(dtf.to_string(index=False))
 
 
+def fill_kyle_cart(session, kyle):
+    kyle_cart = KyleCart()
+    kyle_item_id = input("enter id")
+    while kyle_item_id:
+        kyle_item = session.query(KyleItem).filter(
+            KyleItem.id == kyle_item_id).first()
+        print(kyle_item)
+        if kyle_item in KyleItem.kyle_items:
+            kyle_cart.kyle_items.append(kyle_item)
+        else:
+            print("oh no")
 
 
+
+def fill_cart(session, store):
+    shopping_cart = ShoppingCart(store=store)
+    grocery_item_id = input('Please enter the ID of your first item: ')
+    cart_total = 0
+    while grocery_item_id:
+        grocery_item = session.query(GroceryItem).filter(
+            GroceryItem.id==grocery_item_id).first()
+        if grocery_item in store.grocery_items:
+            shopping_cart.grocery_items.append(grocery_item)
+            cart_total += grocery_item.price
+            print(f'Cart total is now ${cart_total:.2f}\n')
+        else:
+            grocery_item_id = input('Please enter a valid grocery item ID: ')
+            continue
+
+        yes_no = None
+        while yes_no not in YES + NO:
+            yes_no = input('Would you like to add another item to your cart? (Y/n) ')
+            if yes_no.lower() in YES:
+                grocery_item_id = input('Please enter the ID of your next item: ')
+            elif yes_no.lower() in NO:
+                grocery_item_id = None
+
+    return shopping_cart, cart_total
