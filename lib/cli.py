122664d2c48a2de0ea1695e8c01257dc4657564b
Kyle Schneider <kyle.v.schneider.251@gmail.com>
2023-03-08 11:48:02 -0700

    plz db work i beg you

--- lib/cli.py
+++ lib/cli.py
@@ -1,11 +1,14 @@
-from db.models import KyleItem, KyleLog
+from db.models import Base, KyleItem, KyleLog
 import random
 from helpers import create_table
 
 from faker import Faker
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
+
 engine = create_engine('sqlite:///kyle.db')
+Base.metadata.create_all(engine)
+
 Session = sessionmaker(bind=engine)
 session = Session()
 fake = Faker()
@@ -27,6 +30,8 @@ if __name__ == '__main__':
     [first_name, last_name] = name.split(' ')
     if not (first_name.lower() == 'kyle'):
         raise Exception('We cannot permit the sale of Kyle orphans to individuals not named Kyle.')
+    
     print(f"Welcome {name} take a look at our merchandise")
     kyles = session.query(KyleItem)
+    print(kyles.first())
     create_table(kyles)
--- lib/db/seed.py
+++ lib/db/seed.py
@@ -6,7 +6,7 @@ from faker import Faker
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 
-from models import KyleItem, KyleLog
+from models import Base, KyleItem, KyleLog
 
 superpowers = [
     "Annoyance Inducement",
@@ -26,6 +26,8 @@ superpowers = [
 ]
 
 engine = create_engine('sqlite:///kyle.db')
+Base.metadata.create_all(engine)
+
 Session = sessionmaker(bind=engine)
 session = Session()
 fake = Faker()
